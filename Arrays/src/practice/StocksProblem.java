a6e6dcff2fd83b780586c191f34148b6002f3d89
ayush2753 <ayush.21cse203@gmail.com>
2025-03-20 10:39:43 +0530

    Array Completed

---
+++ Arrays/src/practice/StocksProblem.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class StocksProblem {
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(BuySellStock(prices));
+    }
+    public static int BuySellStock(int[] prices){
+        int buyPrice =Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if(buyPrice < prices[i]){
+                int profit = prices[i] - buyPrice; //today's profit
+                maxProfit = Math.max(maxProfit,profit);
+            }
+            else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ Arrays/src/practice/SubArray2.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.sql.SQLOutput;
+
+public class SubArray2 {
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        MaxSubArray(numbers);
+    }
+    public static void MaxSubArray(int numbers[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                currSum = 0;
+                for (int k = start; k <= end; k++) {
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if(maxSum <currSum){
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("max sum = " +maxSum);
+    }
+}
