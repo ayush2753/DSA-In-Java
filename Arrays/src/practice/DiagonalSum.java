a13c072d8728b25158d158a9df16d247e9da8806
ayush2753 <ayush.21cse203@gmail.com>
2025-03-31 09:55:10 +0530

    2D Array

---
+++ Arrays/src/practice/DiagonalSum.java
@@ -0,0 +1,35 @@
+package practice;
+
+public class DiagonalSum {
+    public static void main(String[] args) {
+        int arr [][]  = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16},
+        };
+        System.out.println(Diagonal(arr));
+    }
+    public static int Diagonal(int matrix[][]){
+        int sum =0;
+        /*for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(i == j){
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length-1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }*/
+        for (int i = 0; i < matrix.length; i++) {
+            //pd
+            sum+= matrix[i][i];
+            //sd
+            if(i != matrix.length-1-i )
+            {
+                sum+= matrix[i][matrix.length-i-1];
+            }
+        }
+        return sum;
+    }
+}
